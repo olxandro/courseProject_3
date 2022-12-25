a223288685a4ccfb786fad8829ba077d2dc73cbb
Admin O <olexandro@gmail.com>
2022-12-25 23:07:56 +0300

    Initial commit

---
+++ src/main/java/com/skypro/courseproject_3/CourseProject3Application.java
@@ -0,0 +1,13 @@
+package com.skypro.courseproject_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/courseproject_3/CourseProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.courseproject_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
